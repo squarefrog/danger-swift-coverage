d79829140cd24dd164ff68600cf3f3780f56379f
Paul Williamson <paul.williamson@thebeansgroup.com>
2022-08-11 12:16:02 +0100

    Add concurrent workaround

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
         .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
     ],
     dependencies: [
-        .package(url: "https://github.com/danger/swift.git", from: "3.0.0"),
+        .package(url: "https://github.com/danger/swift.git", .revision("263436d79c51bf32e21b960f387bac4ef2d05c34")),
         // Dev dependencies
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"), // dev
         .package(url: "https://github.com/Realm/SwiftLint", from: "0.28.1"), // dev
         .package(url: "https://github.com/f-meloni/Rocket", from: "1.0.0"), // dev
         .package(url: "https://github.com/orta/Komondor", from: "1.0.0"), // dev
-        .package(url: "https://github.com/f-meloni/danger-swift-xcodesummary", from: "1.0.0"), // dev
+        .package(url: "https://github.com/squarefrog/danger-swift-xcodesummary", .branc("concurrent")), // dev
     ],
     targets: [
         .target(name: "DangerDependencies", dependencies: ["Danger", "DangerSwiftCoverage", "DangerXCodeSummary"]), // dev
